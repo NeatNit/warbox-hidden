4eca76ef0cac47f6dff48f33a3a350116b4c2634
NeatNit <neatnit@gmail.com>
2016-11-28 23:39:03 +0200

    zombies spawn constantly

--- gamemodes/warbox-hidden/gamemode/cl_init.lua
+++ gamemodes/warbox-hidden/gamemode/cl_init.lua
@@ -1,23 +1,23 @@
 include("shared.lua")
 
-local highlight = Vector(0,0,0)
-local hide_vecs = {}
-local ai_vecs = {}
+-- local highlight = Vector(0,0,0)
+-- local hide_vecs = {}
+-- local ai_vecs = {}
 local center_vecs = {}
 net.Receive("testnav", function()
-	highlight = net.ReadVector()
+	-- highlight = net.ReadVector()
 
-	hide_vecs = {}
-	local count = net.ReadInt(32)
-	for i = 1, count do
-		table.insert(hide_vecs, net.ReadVector())
-	end
+	-- hide_vecs = {}
+	-- local count = net.ReadInt(32)
+	-- for i = 1, count do
+	-- 	table.insert(hide_vecs, net.ReadVector())
+	-- end
 
-	ai_vecs = {}
-	count = net.ReadInt(32)
-	for i = 1, count do
-		table.insert(ai_vecs, net.ReadVector())
-	end
+	-- ai_vecs = {}
+	-- count = net.ReadInt(32)
+	-- for i = 1, count do
+	-- 	table.insert(ai_vecs, net.ReadVector())
+	-- end
 
 	center_vecs = {}
 	count = net.ReadInt(32)
@@ -27,18 +27,22 @@ net.Receive("testnav", function()
 end)
 
 hook.Add("PostDrawTranslucentRenderables", "testnav", function()
-	render.SetColorMaterialIgnoreZ()
-	for i = 1, #ai_vecs do
-		render.DrawSphere(ai_vecs[i], 10, 4, 3, Color(255, 0, 0))
-	end
+	-- render.SetColorMaterialIgnoreZ()
+	-- for i = 1, #ai_vecs do
+	-- 	render.DrawSphere(ai_vecs[i], 10, 4, 3, Color(255, 0, 0))
+	-- end
 
-	render.DrawSphere(highlight, 9, 4, 3, Color(0, 0, 255))
+	-- render.DrawSphere(highlight, 9, 4, 3, Color(0, 0, 255))
 
-	for i = 1, #hide_vecs do
-		render.DrawSphere(hide_vecs[i], 8, 4, 3, Color(255, 255, 0))
-	end
+	-- for i = 1, #hide_vecs do
+	-- 	render.DrawSphere(hide_vecs[i], 8, 4, 3, Color(255, 255, 0))
+	-- end
+
+	render.SetColorMaterial()
 
 	for i = 1, #center_vecs do
 		render.DrawSphere(center_vecs[i], 6, 4, 3, Color(0, 255, 0))
+		-- render.DrawSphere(center_vecs[i] + Vector(0, 0, 10), 1, 4, 3, Color(255, 0, 0))
+		-- render.DrawSphere(center_vecs[i] + Vector(0, 0, 20), 1, 4, 3, Color(255, 0, 0))
 	end
 end)
--- gamemodes/warbox-hidden/gamemode/init.lua
+++ gamemodes/warbox-hidden/gamemode/init.lua
@@ -1,43 +1,93 @@
 AddCSLuaFile("cl_init.lua")
 include("shared.lua")
 
+local function PlayerCanSeeNav(nav)
+	local ply = Entity(1)
+	local eyes = ply:EyePos()
+	local floor = nav:GetCenter()
+	return not (
+		util.TraceLine({start = eyes, filter = ply, endpos = floor}).Hit and
+		util.TraceLine({start = eyes, filter = ply, endpos = floor + Vector(0, 0, 10)}).Hit and
+		util.TraceLine({start = eyes, filter = ply, endpos = floor + Vector(0, 0, 20)}).Hit
+	)
+end
+
+--hook.Add("InitPostEntity", "Spawn Zombies Randomly", function()
+	timer.Create("spawn zombies", 2, 0, function()
+		local thisnav = navmesh.GetNavArea(Entity(1):GetPos(), 1000)
+		if not IsValid(thisnav) then return end
+
+		local tried = {}
+
+		while true do
+			if not PlayerCanSeeNav(thisnav) then
+				PrintMessage(HUD_PRINTTALK, "Picked spawn point - player can't see")
+				break
+			end
+
+			tried[thisnav:GetID()] = true
+			local neighbors = thisnav:GetAdjacentAreas()
+			for i = #neighbors, 1, -1 do	-- go backwards so we can table.remove stuff
+				local nav = neighbors[i]
+				if tried[nav:GetID()] then
+					table.remove(neighbors, i)
+					break
+				end
+			end
+
+			if #neighbors == 0 then
+				PrintMessage(HUD_PRINTTALK, "Picked spawn point - no neighbors left")
+				break
+			end
+			thisnav = neighbors[math.random(1, #neighbors)]
+		end
+
+		local zombie = ents.Create("npc_zombie")
+		print(zombie)
+		if not IsValid(zombie) then return end
+		zombie:SetPos(thisnav:GetCenter() + Vector(0, 0, 10))
+		zombie:Spawn()
+	end)
+--end)
+
+
 util.AddNetworkString("testnav")
 
-timer.Create("testnav",2,0,function()
+timer.Create("testnav",10,0,function()
 	local navs = navmesh.GetAllNavAreas()
 
 	print("navs:", #navs)
 
-	local hide_vecs = {}
+	-- local hide_vecs = {}
 	local center_vecs = {}
 	for _, nav in pairs(navs) do
-		for _, vec in pairs(nav:GetHidingSpots()) do
-			table.insert(hide_vecs, vec)
-		end
+		-- for _, vec in pairs(nav:GetHidingSpots()) do
+		-- 	table.insert(hide_vecs, vec)
+		-- end
 
 		table.insert(center_vecs, nav:GetCenter())
 	end
 
-	local ai_vecs = {}
-	for _, node in pairs(ents.FindByClass("ai_hint")) do
-		table.insert(ai_vecs, node:GetPos())
-	end
+	-- local ai_vecs = {}
+	-- for _, node in pairs(ents.FindByClass("ai_hint")) do
+	-- 	table.insert(ai_vecs, node:GetPos())
+	-- end
 
-	local hlnav = navmesh.GetNavArea(Entity(1):GetPos(), 1000)
-	local hlpos = hlnav:GetCenter() or vector_origin
-	print(hlnav, hlpos)
+	-- local hlnav = navmesh.GetNavArea(Entity(1):GetPos(), 1000)
+	-- local hlpos = hlnav:GetCenter() or vector_origin
+	-- print(hlnav, hlpos)
 
 	net.Start("testnav")
-	net.WriteVector(hlpos)
-	net.WriteInt(#hide_vecs, 32)
-		for i = 1, #hide_vecs do
-			net.WriteVector(hide_vecs[i])
-		end
+		-- net.WriteVector(hlpos)
+		-- net.WriteInt(#hide_vecs, 32)
+		-- for i = 1, #hide_vecs do
+		-- 	net.WriteVector(hide_vecs[i])
+		-- end
 
-		net.WriteInt(#ai_vecs, 32)
-		for i = 1, #ai_vecs do
-			net.WriteVector(ai_vecs[i])
-		end
+		-- net.WriteInt(#ai_vecs, 32)
+		-- for i = 1, #ai_vecs do
+		-- 	net.WriteVector(ai_vecs[i])
+		-- end
 
 		net.WriteInt(#center_vecs, 32)
 		for i = 1, #center_vecs do
